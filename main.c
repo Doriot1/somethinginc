33ef3f42f695610f60e2abcf12c9b1b6206b5ccc
doriot1 <daniel.novakeyp@gmail.com>
2015-10-03 13:29:18 +0200

    Hopefully malloc done

--- main.c
+++ main.c
@@ -1,6 +1,7 @@
 #include <stdio.h>
 
 #define HEAD 32
+#define true 1
 
 // 32 bajtov
 typedef struct mem_block {
@@ -11,38 +12,50 @@ typedef struct mem_block {
     struct mem_block *previous;
 } MEM_BLOCK;
 
+
 void *start;
 
-/*
- * explicitny zoznam, implicitny zoznam s first fit, best fit, next fit
- * spravis si
- */
+
+void header_init(MEM_BLOCK *pom1, MEM_BLOCK *pom2){
+    pom1->next = pom2;
+    pom2->previous = pom1;
+    pom2->next = NULL;
+    pom2->heap_size = pom1->heap_size;
+    pom2->isFree = 1;
+    pom2->alloc_size = 0;
+}
+
 
 void *memory_alloc(unsigned int size) {
     MEM_BLOCK *first = (MEM_BLOCK *) start;
     MEM_BLOCK *current = first;
     MEM_BLOCK *new = NULL;
+    short static isFirst = 0;
 
-    if (first->isFree) {
+    // if we want to allocate 0 bytes
+    if (size == 0)
+        return NULL;
+
+    // spusti sa prave raz pri prvej alokacii
+    if (first->isFree && !isFirst) {
         if (first->heap_size < HEAD + size)
             return NULL;
         first->heap_size -= HEAD + size;
         first->isFree = 0;
         first->alloc_size = size;
+        isFirst = 1;
 
         // current bude ukazovat na dalsie volne miesto v pamati
         current = 1 + (MEM_BLOCK *) ((char *) current + size);
-        first->next = current;
-        current->previous = first;
-        current->next = NULL;
-        current->heap_size = first->heap_size;
-        current->isFree = 1;
-        current->alloc_size = 0;
+        header_init(first, current);
         return first + 1;
     }
 
-    while (!current->isFree && (current->alloc_size != 0 && current->alloc_size < size))
+    while (true) {
         current = current->next;
+        if (current->isFree && (current->alloc_size == 0 || current->alloc_size >= size))
+            break;
+    }
 
     if (current->heap_size < HEAD + size)
         return NULL;
@@ -51,60 +64,88 @@ void *memory_alloc(unsigned int size) {
     current->isFree = 0;
     current->alloc_size = size;
 
-    new = 1 + (MEM_BLOCK *) ((char *) current + size);
-    current->next = new;
-    new->previous = current;
-    new->previous = NULL;
-    new->heap_size = current->heap_size;
-    new->isFree = 0;
-    new->alloc_size = 0;
-
+    if (current->heap_size > HEAD && current->next == NULL) {
+        // explain this
+        new = 1 + (MEM_BLOCK *) ((char *) current + size);
+        header_init(current, new);
+    }
 
     return current + 1;
 }
 
-/*
- * 1.st checkne sa pointer v memory check
- *
- */
 
 int memory_free(void *valid_ptr) {
-    return 0;
+    MEM_BLOCK *current = start;
+
+    while (current) {
+        if ((current + 1) == valid_ptr) {
+            current->isFree = 1;
+            current->heap_size += current->alloc_size;
+            if (current->next->isFree) {
+
+            }
+            return 0;
+        }
+        current = current->next;
+    }
+
+    return 1;
 }
 
-/*
- *
- */
 
 int memory_check(void *ptr) {
+    MEM_BLOCK *first = start, *current = start;
+
+    while (current) {
+        if ((current + 1) == ptr)
+            return 1;
+        current = current->next;
+    }
+
     return 0;
 }
+
 
 void memory_init(void *ptr, unsigned int size) {
     start = ptr;
     MEM_BLOCK *first = (MEM_BLOCK *) start;
     first->heap_size = size;
     first->isFree = 1;
-
 }
 
-int main() {
-    char region[100];
-    int i;
-    char *pointer, *pointer2;
-    memory_init(region, 100);
 
-    pointer = (char *) memory_alloc(5);
-    pointer[0] = 'a';
-    pointer[1] = 'b';
-    pointer[2] = 'c';
-    pointer[3] = 'd';
-    pointer[4] = 'e';
-    for (i = 0; i < 5; i++)
-        printf("%c", pointer[i]);
-    pointer2 = (char *) memory_alloc(5);
-    pointer2[0] = 'z';
-    printf("\n%c", pointer2[0]);
+int main() {
+    char region[300];
+    int i;
+    char *pointer1, *pointer2, *pointer3;
+    int *pointer4;
+    memory_init(region, 300);
+
+    pointer1 = (char *) memory_alloc(5);
+
+    /*if (memory_check(pointer1))
+        memory_free(pointer1);
+    else
+        printf("Not correct-o! :(");*/
+      for (i = 0; i < 5; i++) {
+          pointer1[i] = (char) (97 + i);
+          printf("%c", pointer1[i]);
+      }
+
+      pointer2 = (char *) memory_alloc(5);
+      pointer2[0] = 'z';
+      printf("\n%c\n", pointer2[0]);
+
+      pointer3 = (char *) memory_alloc(30);
+      pointer4 = (int *) memory_alloc(sizeof(int)*5);
+
+      fgets(pointer3, 30, stdin);
+      printf("\n%s\n", pointer3);
+
+      for(i = 0; i < 5; i++){
+          pointer4[i] = i * i;
+          printf("%d\n", pointer4[i]);
+      }
 
     return 0;
 }
